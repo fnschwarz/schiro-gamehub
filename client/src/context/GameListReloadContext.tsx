e13860d93c73018db006751e0edb643f24460638
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-04-18 17:32:07 +0200

    Convert project to TypeScript

--- client/src/context/GameListReloadContext.tsx
+++ client/src/context/GameListReloadContext.tsx
@@ -1,5 +1,10 @@
-import { createContext } from "react";
+import { createContext, Dispatch, SetStateAction } from "react";
 
-const GameListReloadContext = createContext(null);
+interface GameListReloadContextType {
+    reloadTrigger: boolean,
+    setReloadTrigger: Dispatch<SetStateAction<boolean>>;
+}
+
+const GameListReloadContext = createContext<GameListReloadContextType | null>(null);
 
 export default GameListReloadContext;
