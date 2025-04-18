a91d22eace88f20b0b9ba1f247f58df7d75c8b34
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-04-18 13:13:43 +0200

    Fix whitespace

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,24 +5,24 @@ import Header from './components/Header/Header.jsx';
 import Section from './components/Section/Section.jsx';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [reloadTrigger, setReloadTrigger] = useState(false);
-  
-  useEffect(() => {
-      (async () => {
-          const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/authenticate`, { credentials: "include" });
-          setIsAuthenticated(response.ok);
-      })();
-  }, []);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [reloadTrigger, setReloadTrigger] = useState(false);
+    
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/authenticate`, { credentials: "include" });
+            setIsAuthenticated(response.ok);
+        })();
+    }, []);
 
-  return(
-    <AuthenticationContext.Provider value = {{ isAuthenticated }}>
-      <GameListReloadContext.Provider value = {{ reloadTrigger, setReloadTrigger }}>
-        <Header />
-        <Section />
-      </GameListReloadContext.Provider>
-    </AuthenticationContext.Provider>
-  );
+    return(
+        <AuthenticationContext.Provider value = {{ isAuthenticated }}>
+            <GameListReloadContext.Provider value = {{ reloadTrigger, setReloadTrigger }}>
+                <Header />
+                <Section />
+            </GameListReloadContext.Provider>
+        </AuthenticationContext.Provider>
+    );
 }
 
 export default App
