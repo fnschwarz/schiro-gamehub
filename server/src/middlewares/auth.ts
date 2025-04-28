334b21ff4364eea374df298087068b9ed3349e3c
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-04-28 17:04:30 +0200

    Extend Request interface to include JwtPayload as user property

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -18,12 +18,17 @@ export const authenticateToken = async (req: Request, res: Response, next: NextF
     }
 
     try {
-        req.body.user = verify(token, secret);
+        req.user = verify(token, secret);
     } catch (error) {
         res.status(403).json({ status: 403, message: 'Invalid or expired token.' }); return;
     }
+
+    if(typeof(req.user) !== 'object'){
+        logError(`Token authentication failed: invalid token payload. Expected 'JwtPayload' but received '${typeof(req.user)}'`);
+        res.status(500).json({ status: 500, message: 'Internal Server Error: invalid token payload.' }); return;
+    }
     
-    const isAuthorizedUser = await User.findOne({ email: req.body.user.email }).catch((error) => { 
+    const isAuthorizedUser = await User.findOne({ email: req.user.email }).catch((error) => { 
         logError('Token authentication failed: database cannot be accessed', error);
         return undefined;
     });
