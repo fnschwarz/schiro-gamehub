508da040b57db50765c048607c98e62d3833361d
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-04-30 19:12:39 +0200

    Wrap a tag around login button to show link

--- client/src/components/auth/twitch/LoginButton/LoginButton.tsx
+++ client/src/components/auth/twitch/LoginButton/LoginButton.tsx
@@ -8,17 +8,21 @@ function LoginButton() {
 
     if(isAuthenticated){
         return (
-            <button className={style['login-button']} onClick={() => window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`}>
-                Logout
-            </button>
+            <a href={`${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`}>
+                <button className={style['login-button']} onClick={() => window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`}>
+                    Logout
+                </button>
+            </a>
         )
     }
 
     return (
-        <button className={style['login-button']} onClick={() => window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/auth/twitch`}>
-            <Icon path={mdiTwitch} size={0.9} />
-            Login with Twitch
-        </button>
+        <a href={`${import.meta.env.VITE_BACKEND_URL || ''}/api/auth/twitch`}>
+            <button className={style['login-button']}>
+                <Icon path={mdiTwitch} size={0.9} />
+                Login with Twitch
+            </button>
+        </a>
     );
 }
 
