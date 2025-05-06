43222952224ee82869155b809091b6d7acd2bd89
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-05-06 21:25:05 +0200

    Revert changes to GameIdInput padding

--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,13 +1,10 @@
-import style from "./Section.module.css";
 import GameIdInput from '../GameIdInput/GameIdInput.tsx';
 import GameList from '../GameList/GameList.tsx';
 
 function Section() {
     return (
         <>
-            <div className={style['section__game-id-input']}>
-                <GameIdInput />
-            </div>
+            <GameIdInput />
             <GameList />
         </>
     );
