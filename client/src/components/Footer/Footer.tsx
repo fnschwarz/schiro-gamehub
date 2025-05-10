42c48f28f5b00543ba47066a4ab3b5e1cf78b8a8
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-05-10 20:58:01 +0200

    Add dynamic footer text based on window width

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
 import style from './Footer.module.css';
 import Icon from '@mdi/react';
 import { mdiFileDocumentOutline, mdiGithub } from '@mdi/js';
 
 function Footer(){
+    const [copyrightText, setCopyrightText] = useState('Copyright © 2025 Finn Schwarz. All rights reserved.');
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 500) {
+                setCopyrightText('© 2025 Finn Schwarz');
+            } else {
+                setCopyrightText('Copyright © 2025 Finn Schwarz. All rights reserved.');
+            }
+        }
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className={style.footer}>
             <div className={style['footer-items-container']}>
-                Copyright &copy; 2025 Finn Schwarz. All rights reserved.
+                {copyrightText}
                 <div>
                     <a title='Impressum' href='/impressum'><Icon path={mdiFileDocumentOutline} size={1} /></a>
                     <a title='GitHub' href='https://github.com/fiscdev'><Icon path={mdiGithub} size={1} /></a>
