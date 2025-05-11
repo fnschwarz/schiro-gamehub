7bba77b8d3809e28f83d6db5ffa24b3bb3763f5f
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-05-11 13:05:48 +0200

    Add visual error messages for client

--- client/src/components/ui/AddGameButton/AddGameButton.tsx
+++ client/src/components/ui/AddGameButton/AddGameButton.tsx
@@ -1,16 +1,20 @@
-import { useContext } from 'react';
+import { ReactElement, useContext, useState } from 'react';
 import style from './AddGameButton.module.css';
 import { API_BASE_URL } from '../../../configs/api.config.ts';
+import Icon from '@mdi/react';
+import { mdiAlertBox } from '@mdi/js';
 import GameListReloadContext from '../../../context/GameListReloadContext.tsx';
 
 function AddGameButton({ isFocused }: client.props.Button){
+    const [errorIcon, setErrorIcon] = useState<ReactElement | null>(null);
+    const [errorMessage, setErrorMessage] = useState<ReactElement | null>(null);
     const { reloadTrigger, setReloadTrigger} = useContext(GameListReloadContext);
 
     const handleClick = async () => {
         const game = {
             id : parseInt((document.getElementById('game-id-input') as HTMLInputElement)?.value || '0')
         }
-    
+
         const response = await fetch(`${API_BASE_URL}/api/games`, {
             method: 'POST',
             credentials: 'include',
@@ -22,14 +26,25 @@ function AddGameButton({ isFocused }: client.props.Button){
         });
 
         if (response.ok) {
+            setErrorIcon(null);
+            setErrorMessage(null);
             setReloadTrigger(!reloadTrigger);
+        } else {
+            setErrorIcon(<Icon path={mdiAlertBox} size={1} />);
+            setErrorMessage(<p>{(await response.json()).message}</p>);
         }
     }
-    
+
     return(
-        <button className={`${style['add-game-button']} ${isFocused ? style.focused : ''}`} onClick={handleClick}>
-            Add Game
-        </button>
+        <div className={style['add-game-button-container']}>
+            <button className={`${style['add-game-button']} ${isFocused ? style.focused : ''}`} onClick={handleClick}>
+                Add Game
+            </button>
+            <div className={style['add-game-button-error']}>
+                {errorIcon}
+                {errorMessage}
+            </div>
+        </div>
     );
 }
 
