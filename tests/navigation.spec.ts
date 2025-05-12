7357ffd8ccfe350a285186ae3f404a760d756242
fiscdev <82593560+Varixus@users.noreply.github.com>
2025-05-12 23:13:26 +0200

    Split navigation test into several tests

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,22 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('testing page navigation', async ({ page }) => {
-    // check NotFound error page
-    await page.goto('http://localhost:5173/thispageisinvalid');
-    await expect(page.locator('text=404 Not Found')).toBeVisible();
-    await page.getByRole('button').click();
-    await expect(page).toHaveURL('http://localhost:5173/');
+test.describe('testing page navigation', () => {
+    test('testing NotFound page', async ({ page }) => {
+        await page.goto('http://localhost:5173/thispageisinvalid');
+        await expect(page.locator('text=404 Not Found')).toBeVisible();
+        await page.getByRole('button').click();
+        await expect(page).toHaveURL('http://localhost:5173/');
+    });
 
-    // check AccessDenied error page
-    await page.goto('http://localhost:5173/access-denied');
-    await expect(page.locator('text=403 Forbidden')).toBeVisible();
-    await page.getByRole('button').click();
-    await expect(page).toHaveURL('http://localhost:5173/');
+    test('testing AccessDenied page', async ({ page }) => {
+        await page.goto('http://localhost:5173/access-denied');
+        await expect(page.locator('text=403 Forbidden')).toBeVisible();
+        await page.getByRole('button').click();
+        await expect(page).toHaveURL('http://localhost:5173/');
+    });
 
-    // check Impressum page
-    await page.locator('a[href="/impressum"]').click();
-    await expect(page).toHaveURL('http://localhost:5173/impressum');
-    await expect(page.getByRole('heading', { name: 'Impressum' })).toBeVisible();
-    await page.locator('a[href="/"]').click();
-    await expect(page).toHaveURL('http://localhost:5173/');
-});
+    test('testing Impressum page', async ({ page }) => {
+        await page.goto('http://localhost:5173/');
+        await page.locator('a[href="/impressum"]').click();
+        await expect(page).toHaveURL('http://localhost:5173/impressum');
+        await expect(page.getByRole('heading', { name: 'Impressum' })).toBeVisible();
+        await page.locator('a[href="/"]').click();
+        await expect(page).toHaveURL('http://localhost:5173/');
+    });
+})
