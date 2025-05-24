90131d5387c40e1983dae93b0c4bac8c8759d51b
fnschwarz <82593560+fnschwarz@users.noreply.github.com>
2025-05-24 17:16:55 +0200

    Add comments to auth util functions

--- server/src/utils/auth.utils.ts
+++ server/src/utils/auth.utils.ts
@@ -2,15 +2,48 @@ import { BACKEND_SERVER_URL, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } from '../c
 import { AppError } from '../errors/error';
 import { User } from '../models/user.model';
 
+/**
+ * Exchanges an authorization code for an access token using the Twitch OAuth2 API.
+ *
+ * @param code - Authorization code received from Twitch after user login.
+ * @returns Promise that resolves to the OAuth access token string.
+ */
 export const getAccessToken = async (code: string): Promise<string> => {
+    /**
+     * Response received from Twitch OAuth2 API containing access token,
+     * expiration time, refresh token, scopes and token type.
+     */
     const tokenResponse = await fetch(`https://id.twitch.tv/oauth2/token`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({
+            /**
+             * You have to register an Twitch application at
+             * [Twitch Developer Console](https://dev.twitch.tv/console).
+             *
+             * You can find a detailed documentation about getting Twitch OAuth Access
+             * Token [here](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/).
+             */
+
+            /**
+             * Your app’s registered client ID.
+             */
             client_id: TWITCH_CLIENT_ID,
+            /**
+             * Your app’s registered client secret.
+             */
             client_secret: TWITCH_CLIENT_SECRET,
+            /**
+             * The code that the `/authorize` response returned in the code query parameter.
+             */
             code: code.toString(),
+            /**
+             * Must be set to `authorization_code`.
+             */
             grant_type: 'authorization_code',
+            /**
+             * Your app’s registered redirect URI.
+             */
             redirect_uri: `${BACKEND_SERVER_URL}/api/auth/twitch/callback`
         }),
     }).catch( (error) => {
@@ -21,9 +54,11 @@ export const getAccessToken = async (code: string): Promise<string> => {
         throw new AppError('UPSTREAM_SERVICE_ERROR', 'get_access_token');
     }
 
+    /**
+     * JSON containing parsed Twitch OAuth2 API response.
+     */
     const responseData = await tokenResponse.json();
 
-    // check if there is a access token in response data and if it has proper type
     if (!responseData || typeof(responseData.access_token) !== 'string') {
         throw new AppError('INVALID_RESPONSE_FORMAT', 'get_access_token');
     }
@@ -31,6 +66,12 @@ export const getAccessToken = async (code: string): Promise<string> => {
     return responseData.access_token;
 }
 
+/**
+ * Exchanges an access token for the users email address using the Twitch OAuth2 API.
+ *
+ * @param accessToken Access token received from Twitch after authorization code exchange.
+ * @returns Promise that resolves to the Twitch users email.
+ */
 export const getUserEmail = async (accessToken: string): Promise<string> => {
     const userResponse = await fetch('https://api.twitch.tv/helix/users', {
         headers: {
@@ -61,6 +102,12 @@ export const getUserEmail = async (accessToken: string): Promise<string> => {
     return userData.data[0].email;
 }
 
+/**
+ * Checks if the given email address is whitelisted.
+ *
+ * @param email - Email address you want to check the whitelist status of.
+ * @returns `true` if a document containing the given email address exists in the database, `false` otherwise.
+ */
 export const isWhitelistedUser = async (email: string): Promise<boolean> => {
     const user = await User.findOne({ email: email }).catch((error) => {
         throw new AppError('DATABASE_CONNECTION_ERROR', 'check_user_whitelist', error);
